75a9bef38ef3eacd44919e91e22b095dc53669d1
ImGabe <gabrielpmonte@hotmail.com>
2021-06-21 15:22:41 -0300

    fix navbar css

--- src/components/Navbar/NavbarLink.js
+++ src/components/Navbar/NavbarLink.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, useRouteMatch } from 'react-router-dom'
-
+import style from './Navbar.module.css'
 function NavbarLink ({ className, label, to, activeOnlyWhenExact }) {
   const match = useRouteMatch({
     path: to,
     exact: activeOnlyWhenExact
   })
 
-  return <Link to={to} className={`${className} ${match && 'active'}`}>{label}</Link>
+  return <Link to={to} className={`${className} ${match && style.active}`}>{label}</Link>
 }
 
 NavbarLink.propTypes = {
